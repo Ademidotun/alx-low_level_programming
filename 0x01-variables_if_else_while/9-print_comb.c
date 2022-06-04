4a7dfe31f885c473549ea5dafd675a73679ca36d
Balogun Adedotun <balodotun@gmail.com>
2022-06-03 18:48:17 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all possible combinations of single-digit numbers
  * Return: Always 0 (Success)
-  */
+ */
 int main(void)
 {
 	int n;
@@ -14,8 +14,8 @@ int main(void)
 			if (n != 57)
 			{
 				putchar(',');
-																putchar(' ');
-															}
+				putchar(' ');
+			}
 	}
 	putchar('\n');
 	return (0);
