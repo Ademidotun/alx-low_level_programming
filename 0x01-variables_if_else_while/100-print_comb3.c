79027ccf90da6f70145cfee6db2540b4b29631b7
Balogun Adedotun <balodotun@gmail.com>
2022-06-03 19:08:52 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,16 +13,16 @@ int main(void)
 		for (m = 49; m <= 57; m++)
 		{
 			if (m > n)
-													{
-														putchar(n);
-														putchar(m);
-														if (n != 56 || m != 57)
-														{
-															putchar(',');
-															putchar(' ');
-														}
-													}
-												}
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
